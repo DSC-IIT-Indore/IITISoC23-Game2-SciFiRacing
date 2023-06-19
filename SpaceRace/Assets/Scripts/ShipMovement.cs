c22f79c9d4f4cf070b6caecd7e8fad5402972dd3
rajat_vishwa <74039825+Rajat-Vishwa@users.noreply.github.com>
2023-06-19 20:36:20 +0530

    forward movement

--- SpaceRace/Assets/Scripts/ShipMovement.cs
+++ SpaceRace/Assets/Scripts/ShipMovement.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class ShipMovement : MonoBehaviour
 {
     public float acceleration = 10f;
-    public float MAX_SPEED = 50f;
+    public float MAX_SQR_SPEED = 2500f;
+    private Rigidbody rb;
     private Vector3 inputData = Vector3.zero;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        inputData = new Vector3(h, v, 0);
+        float z = Input.GetButtonDown("L Shift") ? 1 : 0;
+        inputData = new Vector3(h, v, z);
+    }
+
+    void FixedUpdate()
+    {
+        if(inputData.z == 1 && rb.velocity.sqrMagnitude < MAX_SQR_SPEED){
+            rb.AddForce(transform.forward * acceleration);
+        }
     }
 }
