06944103da09aee0ffc71e1d26200319e9aee1f6
rajat_vishwa <74039825+Rajat-Vishwa@users.noreply.github.com>
2023-07-26 23:09:31 +0530

    Updates

--- SpaceRace/Assets/Scripts/MainMenu.cs
+++ SpaceRace/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI highScoreText;
+
+    void Start()
+    {
+        highScoreText.text = "High Score: " + PlayerPrefs.GetFloat("HighScore").ToString("0") + " m";
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
